5dc0a309680e9a5af7f9ccd7e3757f5730f8a328
Diana Wehbi <dianawehbiii@gmail.com>
2025-08-17 13:07:13 +0300

    Update web.php

--- hapster-backend/routes/web.php
+++ hapster-backend/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::apiResource('products', ProductController::class)->parameters([
+    'products' => 'product'
+]);
+
+Route::apiResource('orders', OrderController::class)->only(['index', 'show', 'store']);
+
